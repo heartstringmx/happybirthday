35444f76ac7de11ebdf3756d963cec8b772744f7
夜羲和 <34050924+heartstringmx@users.noreply.github.com>
2021-10-03 20:53:55 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="吴艳秋" &&  pwd=="0828"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("Wrong Password");
+	}
+});
